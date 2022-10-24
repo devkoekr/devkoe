f892740bc3af29426ceed83648ed39097370f210
devkoekr <116492205+devkoekr@users.noreply.github.com>
2022-10-24 22:21:53 +0900

    Create constandlet.js

---
+++ constandlet.js
@@ -0,0 +1,14 @@
+const a = 15;
+const b = 5;
+
+const CdnServName = "cdnkoe ";
+let CdnHello = "hello";
+
+console.log(a * b);
+console.log(a / b);
+console.log(a + b);
+console.log(CdnServName + CdnHello);
+CdnHello = "cdnhello1";
+console.log(CdnServName + CdnHello);
+
+const와 let
