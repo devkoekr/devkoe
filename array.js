8885a7e45f6d7449c62b1cd69e648cb981d37891
devkoekr <116492205+devkoekr@users.noreply.github.com>
2022-10-24 22:22:17 +0900

    Create array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+const arraysample = [1, 12, 54325, 453542435, 542356265236, true, false, null, "helper"];
+
+console.log(arraysample);
+console.log(arraysample[5]);
+
+arraysample.push(1561654687498748);
+console.log(arraysample);
+
+//array
