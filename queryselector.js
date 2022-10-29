bf48f1b9dac8be2a068d0350e2d47690d15f92c3
devkoekr <116492205+devkoekr@users.noreply.github.com>
2022-10-29 14:54:33 +0900

    Create queryselector.js

---
+++ queryselector.js
@@ -0,0 +1,8 @@
+const title = document.querySelector(".text-center h1");
+title.innerText = "linked out";
+console.log(title);
+
+
+const titlesample = document.getElementById("titlesample");
+titlesample.innerText = "no linked";
+console.log(titlesample);
